0925a1160ef592cddc40a2c1e2629c8943ddad36
maslowind <bart1885@gmail.com>
2019-10-17 11:46:14 +0300

    make it better

--- bot.js
+++ bot.js
@@ -13,16 +13,9 @@ const { BotkitCMSHelper } = require('botkit-plugin-cms');
 const { FacebookAdapter, FacebookEventTypeMiddleware } = require('botbuilder-adapter-facebook');
 
 const { MongoDbStorage } = require('botbuilder-storage-mongodb');
+
 // Load process.env values from .env file
 require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express().use(bodyParser.json()); // creates express http server
-
-
-
-  
-
 
 let storage = null;
 if (process.env.MONGO_URI) {
@@ -46,36 +39,9 @@ const adapter = new FacebookAdapter({
 // emit events based on the type of facebook event being received
 adapter.use(new FacebookEventTypeMiddleware());
 
-app.post('/webhook', (req, res) => {  
-    let body = req.body;
-    if (body.object === 'page') {
-        body.entry.forEach(function(entry) {
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-      });      
-      res.status(200).send('EVENT_RECEIVED');
-    } else {
-      res.sendStatus(404);
-    }  
-  });
-  app.get('/webhook', (req, res) => {
-    let VERIFY_TOKEN = process.env.FACEBOOK_VERIFY_TOKEN;     
-    
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-      
-    if (mode && token) {    
-      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-        console.log('WEBHOOK_VERIFIED');
-        res.status(200).send(challenge);      
-      } else {
-        res.sendStatus(403);      
-      }
-    }
-  });
+
 const controller = new Botkit({
-    webhook_uri: '/webhook',
+    webhook_uri: 'https://webhookfbbotmaslowind.herokuapp.com/webhook',
 
     adapter: adapter,
 
@@ -129,7 +95,7 @@ controller.ready(() => {
 controller.webserver.get('/', (req, res) => {
 
     res.send(`This app is running Botkit ${ controller.version }.`);
- });
+});
 
 
 
