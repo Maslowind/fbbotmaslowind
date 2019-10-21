890899632aaa2fba2719eabd120606732be5e4e6
maslowind <bart1885@gmail.com>
2019-10-21 13:25:02 +0300

    make it better

--- features/main_menu.js
+++ features/main_menu.js
@@ -1,32 +1,56 @@
 module.exports = function(controller) {
-function menu()
-{
-  return   bot.reply(message, {
-        text: 'Here is a menu!',
-        quick_replies: [
-            {
-                title: "My purchases",
-                payload: "my-purchases"
-                
-            },
-            {
-                title: "Shop",
-                payload: "shop"
-            },
-            {
-                title: "Favorites",
-                payload: "favorites"
-            },
-            {
-                title: "To invite a friend",
-                payload: "to-invite-a-friend"
-            }
-        ]
-    });
-}
+ 
     controller.hears('Main menu','message',  async(bot, message) => { 
-     menu();
+        await bot.reply(message, {
+            text: 'Here is a menu!',
+            quick_replies: [
+                {
+                    title: "My purchases",
+                    payload: "my-purchases"
+                    
+                },
+                {
+                    title: "Shop",
+                    payload: "shop"
+                },
+                {
+                    title: "Favorites",
+                    payload: "favorites"
+                },
+                {
+                    title: "To invite a friend",
+                    payload: "to-invite-a-friend"
+                }
+            ]
+        });
+
+    });
+    controller.on('facebook_postback', function(bot, message) {
+
+        if (message.payload == 'Main menu') {
+        await bot.reply(message, {
+            text: 'Here is a menu!',
+            quick_replies: [
+                {
+                    title: "My purchases",
+                    payload: "my-purchases"
+                    
+                },
+                {
+                    title: "Shop",
+                    payload: "shop"
+                },
+                {
+                    title: "Favorites",
+                    payload: "favorites"
+                },
+                {
+                    title: "To invite a friend",
+                    payload: "to-invite-a-friend"
+                }
+            ]
+        });
+    }
     });
     
-
 }
