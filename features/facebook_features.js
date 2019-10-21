9068fbf2137947e151b8ab08815d9491762a4d58
maslowind <bart1885@gmail.com>
2019-10-21 12:32:03 +0300

    make it better

--- features/facebook_features.js
+++ features/facebook_features.js
@@ -13,7 +13,7 @@ module.exports = function(controller) {
     });
 
     controller.on('facebook_postback', async(bot, message) => {
-        await bot.reply(message,`I heard you posting back a post_back about ${ message.text }`);
+        await bot.reply(message,` ${ message.text }`);
     });
 
 
