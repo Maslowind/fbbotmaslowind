a33083fc4271cadd6d1d553b01a4d82fe5cd4baf
maslowind <bart1885@gmail.com>
2019-10-23 15:07:28 +0300

    make it better

--- features/facebook_features.js
+++ features/facebook_features.js
@@ -16,15 +16,7 @@ module.exports = function(controller) {
    /* controller.on('facebook_postback', async(bot, message) => {
         await bot.reply(message,`I heard you posting back a post_back about ${ message.text }`);
     });*/
-    controller.on('facebook_referral', async(bot, message) => {
-        await bot.reply(message,`sender.id: ${ message.sender.id }`);
-        await bot.reply(message,`recipient.id: ${ message.recipient.id }`);
-        await bot.reply(message,`referral.source: ${ message.referral.source }`);
-        await bot.reply(message,`referral.type: ${ message.referral.type }`);
-        await bot.reply(message,`referral.ref: ${ message.referral.ref }`);
-        await bot.reply(message,`referral.referer_uri: ${ message.referral.referer_uri }`);
-
-    });
+    
 
     
 }
---
+++ features/to-invite-a-friend.js
@@ -0,0 +1,29 @@
+module.exports = function(controller) {
+    let to_invite_a_friend_menu = [
+        {
+            title: "Go back",
+            payload: "main-menu"
+            
+        }
+        
+    ];
+    controller.hears('To invite a friend','message',  async(bot, message) => { 
+        await bot.reply(message,`Here is your link for friends: https://m.me/105232900896676?ref=some_random_string`);
+        await bot.reply(message, {
+            text: 'Send link to 3 friends and get one product is free!',
+            quick_replies: to_invite_a_friend_menu
+        });
+    });
+
+    controller.on('facebook_referral', async(bot, message) => {
+        await bot.reply(message,`sender.id: ${ message.sender.id }`);
+        await bot.reply(message,`recipient.id: ${ message.recipient.id }`);
+        await bot.reply(message,`referral.source: ${ message.referral.source }`);
+        await bot.reply(message,`referral.type: ${ message.referral.type }`);
+        await bot.reply(message,`referral.ref: ${ message.referral.ref }`);
+        await bot.reply(message,`referral.referer_uri: ${ message.referral.referer_uri }`);
+
+    });
+
+    
+}
