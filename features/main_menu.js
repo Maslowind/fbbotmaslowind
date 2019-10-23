fe4e6b811346f011214ed1ff35ebcc0f1628e1e6
maslowind <bart1885@gmail.com>
2019-10-23 15:22:32 +0300

    make it better

--- features/main_menu.js
+++ features/main_menu.js
@@ -18,7 +18,7 @@ module.exports = function(controller) {
             payload: "to-invite-a-friend"
         }
     ];
-    controller.hears('Main menu','message',  async(bot, message) => { 
+    controller.hears('Main menu'|| message.attachments.payload=='main-menu','message',  async(bot, message) => { 
 
         await bot.reply(message, {
             text: 'Here is a menu!',
