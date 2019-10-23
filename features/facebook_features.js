6830831815ce627ecf76397144f90a33fcbb4df7
maslowind <bart1885@gmail.com>
2019-10-23 11:15:51 +0300

    make it better

--- features/facebook_features.js
+++ features/facebook_features.js
@@ -13,7 +13,9 @@ module.exports = function(controller) {
     controller.hears(async(message) => message.sticker_id, 'message', async(bot, message) => {
         await bot.reply(message,'Cool sticker.');
     });
-
+    controller.on('facebook_postback', async(bot, message) => {
+        await bot.reply(message,`I heard you posting back a post_back about ${ message.text }`);
+    });
 
 
     
