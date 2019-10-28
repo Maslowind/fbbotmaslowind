ab4727d3b92832b9be6bbc753edbb8b2b4be2261
maslowind <bart1885@gmail.com>
2019-10-28 14:34:50 +0200

    make it better

---
+++ features/choose_product_menu.js
@@ -0,0 +1,20 @@
+module.exports = function(controller) {
+         let bby = require('bestbuy')('TGp7jkZIbKOzfRTDzkofjo2O');
+         
+
+         
+    controller.hears(async(message) => { return (message.quick_reply.payload=='movie'||
+    message.quick_reply.payload=='black-tie'||
+    message.quick_reply.payload=='hard-good'||
+    message.quick_reply.payload=='music'||
+    message.quick_reply.payload=='software'||
+    message.quick_reply.payload=='bundle'||
+    message.quick_reply.payload=='game'
+    )}, 'message', async(bot, message) => { 
+        bby.products(`type="${ message.quick_reply.payload }"`,{show:"image",pageSize:1}).then(function(data){
+            await bot.reply(message,` ${data.products[0].image }`);
+            });
+        
+ });
+
+}
--- features/shop_menu.js
+++ features/shop_menu.js
@@ -50,15 +50,6 @@ module.exports = function(controller) {
         });
         }
     });
-    controller.hears(async(message) => { return (message.quick_reply.payload=='movie'||
-    message.quick_reply.payload=='black-tie'||
-    message.quick_reply.payload=='hard-good'||
-    message.quick_reply.payload=='music'||
-    message.quick_reply.payload=='software'||
-    message.quick_reply.payload=='bundle'||
-    message.quick_reply.payload=='game'
-    )}, 'message', async(bot, message) => { 
-        await bot.reply(message,`I heard you wanna choose ${ message.quick_reply.payload }`);
- });
+
 
 }
