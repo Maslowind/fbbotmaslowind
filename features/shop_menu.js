eec287c2caee32378527082e542ce38b40ab1456
maslowind <bart1885@gmail.com>
2019-10-29 11:38:22 +0200

    make it better

--- features/shop_menu.js
+++ features/shop_menu.js
@@ -52,6 +52,26 @@ module.exports = function(controller) {
         });
         }
     });
-    
+    controller.hears(async(message) => { return (message.quick_reply.payload=='Movie'||
+    message.quick_reply.payload=='BlackTie'||
+    message.quick_reply.payload=='HardGood'||
+    message.quick_reply.payload=='Music'||
+    message.quick_reply.payload=='Software'||
+    message.quick_reply.payload=='Bundle'||
+    message.quick_reply.payload=='Game'
+    )}, 'message', async(bot, message) => { 
+        /*bby.products(`type="${ message.quick_reply.payload }"`,{show:"image"}).then(function(data){});
+        for(let i=0;i<10;i++)
+        {
+        await bot.reply(message,` ${ data.products[i].image }`);
+        }*/
+       /* bby.products(`type="Movie"`,{show:"image"}).then(function(data){});
+        for(let i=0;i<10;i++)
+        {
+        await bot.reply(message,` ${ data.products[i].image }`);
+        }*/
+        await bot.reply(message,` ${ message.quick_reply.payload }`);
+
+ });
 
 }
